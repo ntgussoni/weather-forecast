0da981028862fa5278f0ce0a8a3bcfb890052264
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2019-09-10 19:46:08 -0300

    Scrolls to top on every route transition

--- app/pods/base-route/route.js
+++ app/pods/base-route/route.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 import RouteQueueMixin from 'weather-forecast/mixins/router-promise-queue';
 
-export default Route.extend(RouteQueueMixin, {});
+export default Route.extend(RouteQueueMixin, {
+	activate: function() {
+		this._super();
+		window.scrollTo(0, 0);
+	}
+});
